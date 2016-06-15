3c89213afaf4826fdd8cbc84489e7783dac81832
cBrauge <braugecorentin@gmail.com>
2016-06-15 20:31:11 +0200

    dataaccess: product, account

---
+++ LBCFUBL_WCF/DataAccess/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LBCFUBL_WCF.DataAccess
+{
+    public class Account
+    {
+        public List<DBO.Account> GetAccountsForLogin(String login)
+        {
+            return DBO.DatabaseContext.getInstance().Accounts.Where(a => a.login.Equals(login)).ToList();
+        }
+        public List<DBO.Account> GetAccountsForLoginBeforeDate(String login, DateTime date)
+        {
+            return DBO.DatabaseContext.getInstance().Accounts.Where(a => a.login.Equals(login) && a.date <= date).ToList();
+        }
+        public List<DBO.Account> GetAccountsForLoginAfterDate(String login, DateTime date)
+        {
+            return DBO.DatabaseContext.getInstance().Accounts.Where(a => a.login.Equals(login) && a.date >= date).ToList();
+        }
+        public void CreateAccount(String login, float money, DateTime date)
+        {
+            DBO.Account account = new DBO.Account
+            {
+                login = login,
+                argent = money,
+                date = date
+            };
+            DBO.DatabaseContext.getInstance().Accounts.Add(account);
+            DBO.DatabaseContext.getInstance().SaveChanges();
+        }
+
+        public bool DeleteAccount(String login, DateTime date)
+        {
+            DBO.Account exists = DBO.DatabaseContext.getInstance().Accounts.Where(a => a.login.Equals(login)).Single();
+            if (exists == null)
+                return false;
+            DBO.DatabaseContext.getInstance().Accounts.Remove(exists);
+            return true;
+        }
+    }
+}
---
+++ LBCFUBL_WCF/DataAccess/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LBCFUBL_WCF.DataAccess
+{
+    public class Product
+    {
+        public DBO.Product GetProductFromName(String name)
+        {
+            return DBO.DatabaseContext.getInstance().Products.Where(p => p.name.Equals(name)).Single();
+        }
+
+        public DBO.Product CreateProduct(String name, String description, float cost_without_margin, float cost_with_margin)
+        {
+            DBO.Product exists = GetProductFromName(name);
+            if (exists != null)
+                return exists;
+            DBO.Product product = new DBO.Product
+            {
+                 name = name,
+                 description = description,
+                 cost_without_margin = cost_without_margin,
+                 cost_with_margin = cost_with_margin
+            };
+            DBO.DatabaseContext.getInstance().Products.Add(product);
+            DBO.DatabaseContext.getInstance().SaveChanges();
+            return GetProductFromName(name);
+        }
+
+        public bool DeleteProduct(String name)
+        {
+            DBO.Product exists = GetProductFromName(name);
+            if (exists == null)
+                return false;
+            DBO.DatabaseContext.getInstance().Products.Remove(exists);
+            return true;
+        }
+    }
+}
