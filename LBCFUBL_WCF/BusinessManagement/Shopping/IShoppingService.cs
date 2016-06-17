01a85c37542b9ed837f5bdd37bebac31da09f5bc
cBrauge <braugecorentin@gmail.com>
2016-06-17 19:30:17 +0200

    API: add ShoppingService

---
+++ LBCFUBL_WCF/BusinessManagement/Shopping/IShoppingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LBCFUBL_WCF.BusinessManagement.Shopping
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IShoppingService" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IShoppingService
+    {
+        [OperationContract]
+        List<DBO.Shopping> GetShoppings();
+        [OperationContract]
+        List<DBO.Shopping> GetShoppingsBeforeDate(DateTime date);
+        [OperationContract]
+        List<DBO.Shopping> GetShoppingsAfterDate(DateTime date);
+        [OperationContract]
+        void CreateShopping(DateTime date, List<DBO.Shopping_Product> shopping_products);
+        [OperationContract]
+        bool DeleteShopping(DateTime date);
+    }
+}
---
+++ LBCFUBL_WCF/BusinessManagement/Shopping/ShoppingService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using LBCFUBL_WCF.DBO;
+
+namespace LBCFUBL_WCF.BusinessManagement.Shopping
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ShoppingService" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez ShoppingService.svc ou ShoppingService.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class ShoppingService : IShoppingService
+    {
+        private DataAccess.Shopping shopping;
+        public ShoppingService()
+        {
+            shopping = new DataAccess.Shopping();
+        }
+        public void CreateShopping(DateTime date, List<Shopping_Product> shopping_products)
+        {
+            shopping.CreateShopping(date, shopping_products);
+        }
+
+        public bool DeleteShopping(DateTime date)
+        {
+            return shopping.DeleteShopping(date);
+        }
+
+        public List<DBO.Shopping> GetShoppings()
+        {
+            return shopping.GetShoppings();
+        }
+
+        public List<DBO.Shopping> GetShoppingsAfterDate(DateTime date)
+        {
+            return shopping.GetShoppingsAfterDate(date);
+        }
+
+        public List<DBO.Shopping> GetShoppingsBeforeDate(DateTime date)
+        {
+            return GetShoppingsBeforeDate(date);
+        }
+    }
+}
