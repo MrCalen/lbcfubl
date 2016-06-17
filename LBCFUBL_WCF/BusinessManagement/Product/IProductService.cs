2502f0fda9ca59f65c0a044ff48e867f8e33bf57
cBrauge <braugecorentin@gmail.com>
2016-06-17 18:13:14 +0200

    API: Add ProductService Reference

---
+++ LBCFUBL_WCF/BusinessManagement/Product/IProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LBCFUBL_WCF.BusinessManagement.Product
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IProductService" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        DBO.Product GetProductFromName(String name);
+        [OperationContract]
+        List<DBO.Product> GetAllProducts();
+        [OperationContract]
+        DBO.Product CreateProduct(String name, String description, float cost_without_margin, float cost_with_margin);
+        [OperationContract]
+        bool DeleteProduct(String name);
+    }
+}
---
+++ LBCFUBL_WCF/BusinessManagement/Product/ProductService.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LBCFUBL_WCF.BusinessManagement.Product
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ProductService" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez ProductService.svc ou ProductService.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class ProductService : IProductService
+    {
+        private DataAccess.Product product;
+        ProductService()
+        {
+            product = new DataAccess.Product();
+        }
+        public DBO.Product CreateProduct(string name, string description, float cost_without_margin, float cost_with_margin)
+        {
+            return product.CreateProduct(name, description, cost_without_margin, cost_with_margin);
+        }
+
+        public bool DeleteProduct(string name)
+        {
+            return product.DeleteProduct(name);
+        }
+
+        public List<DBO.Product> GetAllProducts()
+        {
+            return product.GetAllProducts();
+        }
+
+        public DBO.Product GetProductFromName(string name)
+        {
+            return product.GetProductFromName(name);
+        }
+    }
+}
