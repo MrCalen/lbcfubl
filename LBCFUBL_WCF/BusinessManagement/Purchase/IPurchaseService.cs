ed35349931997881050e21f0afcf288d88529f83
cBrauge <braugecorentin@gmail.com>
2016-06-17 19:23:18 +0200

    API: add PurchaseServiceReference

---
+++ LBCFUBL_WCF/BusinessManagement/Purchase/IPurchaseService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LBCFUBL_WCF.BusinessManagement.Purchase
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IPurchaseService" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IPurchaseService
+    {
+        [OperationContract]
+        List<DBO.Purchase> GetPurchasesForLogin(String login);
+        [OperationContract]
+        List<DBO.Purchase> GetPurchasesForLoginBeforeDate(String login, DateTime date);
+        [OperationContract]
+        List<DBO.Purchase> GetPurchasesForLoginAfterDate(String login, DateTime date);
+        [OperationContract]
+        void CreatePurchase(String login, DateTime date, Guid id_prod);
+        [OperationContract]
+        bool DeletePurchase(String login, DateTime date);
+    }
+}
---
+++ LBCFUBL_WCF/BusinessManagement/Purchase/PurchaseService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using LBCFUBL_WCF.DBO;
+
+namespace LBCFUBL_WCF.BusinessManagement.Purchase
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "PurchaseService" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez PurchaseService.svc ou PurchaseService.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class PurchaseService : IPurchaseService
+    {
+        private DataAccess.Purchase purchase;
+        public PurchaseService()
+        {
+            purchase = new DataAccess.Purchase();
+        }
+        public void CreatePurchase(string login, DateTime date, Guid id_prod)
+        {
+            purchase.CreatePurchase(login, date, id_prod);
+        }
+
+        public bool DeletePurchase(string login, DateTime date)
+        {
+            return purchase.DeletePurchase(login, date);
+        }
+
+        public List<DBO.Purchase> GetPurchasesForLogin(string login)
+        {
+            return purchase.GetPurchasesForLogin(login);
+        }
+
+        public List<DBO.Purchase> GetPurchasesForLoginAfterDate(string login, DateTime date)
+        {
+            return purchase.GetPurchasesForLoginAfterDate(login, date);
+        }
+
+        public List<DBO.Purchase> GetPurchasesForLoginBeforeDate(string login, DateTime date)
+        {
+            return purchase.GetPurchasesForLoginBeforeDate(login, date);
+        }
+    }
+}
